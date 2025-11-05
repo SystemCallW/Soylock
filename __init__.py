5345dfa6c54075f73b02980022fbf1e99d3dfc50
SystemCallW <242106863+SystemCallW@users.noreply.github.com>
2025-11-05 11:38:15 -0500

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+""" Soylock Module
+
+This module contains the main logic to search for usernames at social
+networks.
+
+"""
+
+# This variable is only used to check for ImportErrors induced by users running as script rather than as module or package
+import_error_test_var = None
+
+__shortname__   = "Soylock"
+__longname__    = "Soylock: Find Usernames Across Social Networks"
+__version__     = "0.16.3"
+
+forge_api_latest_release = "https://api.github.com/repos/SystemCallW/Soylock/releases/latest"
