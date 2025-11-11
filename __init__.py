bcfb33c7b37b85d42f97abae82764ee4deabbdb6
SystemCallW <242106863+SystemCallW@users.noreply.github.com>
2025-11-11 18:33:29 -0500

    version update

--- __init__.py
+++ __init__.py
@@ -10,6 +10,6 @@ import_error_test_var = None
 
 __shortname__   = "Soylock"
 __longname__    = "Soylock: Find Usernames Across Social Networks"
-__version__     = "0.16.4"
+__version__     = "0.16.5"
 
 forge_api_latest_release = "https://api.github.com/repos/SystemCallW/Soylock/releases/latest"
